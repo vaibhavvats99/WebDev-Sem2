f579eab5fd0cf43a218bfcf018691373c3e4db56
Vaibhav Vats <vaibhav@Vaibhavs-MacBook-Pro-2.local>
2025-01-07 10:13:59 +0530

    Beginning of JavaScript

---
+++ lecture_1/script.js
@@ -0,0 +1,18 @@
+
+
+//Logging things in Javascript
+
+
+console.log('Ram Ram ji')
+
+
+var a = 25;                                         // Using var        ( it can be re-declare and redefine )
+let college = 'Newton School of Technology';        // Using let
+const my_name = 'Vaibhav Vats';                     // Using const
+
+console.log(a)
+console.log(college)
+console.log(my_name)
+
+
+console.log(a,college,my_name)
